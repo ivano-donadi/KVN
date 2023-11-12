5c63fbfdec3e24521f8f743a615919449dc51523
Ivano Donadi <donadi.ivano@gmail.com>
2023-11-12 08:42:01 +0100

    TTD Dataset

--- pvnet/lib/evaluators/make_evaluator.py
+++ pvnet/lib/evaluators/make_evaluator.py
@@ -2,16 +2,16 @@ import imp
 import os
 
 
-def _evaluator_factory(cfg):
+def _evaluator_factory(cfg, **kwargs):
     task = cfg.task
     module = '.'.join(['lib.evaluators', 'custom', task])
     path = os.path.join('lib/evaluators', 'custom', task+'.py')
-    evaluator = imp.load_source(module, path).Evaluator(cfg.result_dir)
+    evaluator = imp.load_source(module, path).Evaluator(cfg.result_dir, **kwargs)
     return evaluator
 
 
-def make_evaluator(cfg):
+def make_evaluator(cfg, **kwargs):
     if cfg.skip_eval:
         return None
     else:
-        return _evaluator_factory(cfg)
+        return _evaluator_factory(cfg, **kwargs)
