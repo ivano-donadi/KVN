92e60c61afc6e989565b27538aac97efbc40dcae
Alberto Pretto <alberto.pretto@gmail.com>
2024-02-28 18:22:54 +0100

    Added output argument where put results file

--- pvnet/pvnet_eval_parallel.py
+++ pvnet/pvnet_eval_parallel.py
@@ -4,6 +4,7 @@ import os
 import argparse
 import json
 import tqdm
+import numpy as np
 
 from lib.networks.pvnet.resnet18  import get_res_pvnet
 from lib.evaluators import make_evaluator
@@ -20,6 +21,10 @@ if __name__ == "__main__":
                         help='Input directory containing the test dataset', required=True)    
     parser.add_argument('-m', '--model',  
                         help='KVNet trained model', required=True)
+    parser.add_argument('-o', '--output_dir',  
+                        help='Output dir')
+    parser.add_argument('--num_iters',  
+                        help='Number of evaluation iterations', default=10)
     parser.add_argument("--cfg_file", 
                         help='Low level configuration file, DO NOT CHANGE THIS PARAMETER IF YOU ARE NOT SURE (default = configs/custom_dsac.yaml)', 
                         default="configs/custom_dsac.yaml", type=str)
@@ -30,7 +35,7 @@ if __name__ == "__main__":
     cfg = make_cfg(args)
 
     cfg.test.dataset_dir = args.dataset_dir;
-
+    num_iters = args.num_iters
     setup_cfg(cfg)
       
     network = get_res_pvnet(cfg.heads.vote_dim, cfg.heads.seg_dim, cfg.train, cfg.test.ransac_rounds).cuda()
@@ -39,10 +44,15 @@ if __name__ == "__main__":
     network.eval()
 
     data_loader = make_data_loader(cfg, is_train=False, json_fn="test.json", LR_split='')
-    evaluator = make_evaluator(cfg, dataset_dir=cfg.test.dataset_dir, is_train=False, suffix='', json_fn = 'test.json')
 
 
-    for batch in tqdm.tqdm(data_loader):
+    proj2d_sum = 0
+    add_sum = 0
+    cm2_sum = 0
+    iters = 0
+    while iters < num_iters:
+      evaluator = make_evaluator(cfg, dataset_dir=cfg.test.dataset_dir, is_train=False, suffix='', json_fn = 'test.json')
+      for batch in tqdm.tqdm(data_loader):
         
         for k in batch:
             if k != 'meta':
@@ -51,4 +61,29 @@ if __name__ == "__main__":
         with torch.no_grad():
             output = network(batch=batch)
         evaluator.joint_evaluate(output,batch)
-    evaluator.summarize()
+    
+      cur_proj2d = np.mean(evaluator.proj2d_it)
+      cur_add = np.mean(evaluator.add_it)
+      cur_cm2 = np.mean(evaluator.cm2_it)
+
+      proj2d_sum += cur_proj2d
+      add_sum += cur_add
+      cm2_sum += cur_cm2
+      iters += 1
+      #evaluator.summarize()
+      
+    if args.output_dir is not None :
+      result_file_name = os.path.join(args.output_dir, 'test.txt')
+      try:
+          with open(result_file_name, 'w') as file:
+            file.write('Final 2d projections metric (iterative pnp): {}\n'.format(proj2d_sum/num_iters))
+            file.write('Final ADD metric (iterative PnP): {}\n'.format(add_sum/num_iters))
+            file.write('Final <2cm metric (iterative pnp): {}\n'.format(cm2_sum/num_iters))  
+            file.write('{}\t{}\t{}\n'.format(proj2d_sum/num_iters,add_sum/num_iters,cm2_sum/num_iters))  
+            
+      except Exception as e:
+          print("Error:", e)
+        
+    print('Final 2d projections metric (iterative pnp): {}'.format(proj2d_sum/num_iters))
+    print('Final ADD metric (iterative PnP): {}'.format(add_sum/num_iters))
+    print('Final <2cm metric (iterative pnp):: {}'.format(cm2_sum/num_iters))
