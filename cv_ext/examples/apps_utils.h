2a663299f632a515e964d29a7e33630afc707a6d
Alberto Pretto <alberto.pretto@gmail.com>
2024-02-29 20:12:33 +0100

    Removed useless files

--- cv_ext/examples/apps_utils.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <vector>
-#include <string>
-#include <opencv2/opencv.hpp>
-
-bool readFileNames ( const std::string &input_file_name, std::vector<std::string> &names );
-bool readFileNames ( const std::string &input_file_name, std::vector< std::vector<std::string> > &names );
-bool readFileNamesFromFolder( const std::string& input_folder_name, std::vector< std::string >& names );
